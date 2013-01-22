d27ed4c1f5460763cbda8d7668a06a45f23ee29b
Philip Arndt <parndt@gmail.com>
2013-01-22 13:14:28 +1300

    Refinery::Page#all_page_part_content can now be decorated on.

--- app/decorators/models/refinery/page_decorator.rb
+++ app/decorators/models/refinery/page_decorator.rb
@@ -4,4 +4,9 @@ Refinery::Page.class_eval do
   # Docs for acts_as_indexed https://github.com/dougal/acts_as_indexed
   acts_as_indexed :fields => [:title, :meta_description,
                               :menu_title, :browser_title, :all_page_part_content]
+
+  # Used to index all the content on this page so it can be easily searched.
+  def all_page_part_content
+    parts.map(&:body).join " "
+  end
 end if defined?(Refinery::Page)
