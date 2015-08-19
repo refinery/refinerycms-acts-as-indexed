21b5711b778d2211c7b48cc742eae39b7a04bc66
Brice Sanchez <brice@bisscomm.com>
2015-08-19 15:56:19 -0400

    Fix user model with new authentication

--- app/decorators/models/refinery/user_decorator.rb
+++ app/decorators/models/refinery/user_decorator.rb
@@ -1,7 +1,7 @@
 require 'acts_as_indexed'
 
 begin
-  Refinery::User.class_eval do
+  Refinery::Authentication::Devise::User.class_eval do
     # Docs for acts_as_indexed http://github.com/dougal/acts_as_indexed
     acts_as_indexed :fields => [:username, :email]
   end
